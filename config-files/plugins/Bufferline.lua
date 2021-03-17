bd4de8add7334e4cbdbccec0af1ffb88bbe5a721
Thomas Schmieck <mail@schmieck.de>
2021-03-18 00:26:15 +0100

    Disable default mappings and diagnostics in bufferline

--- config-files/plugins/Bufferline.lua
+++ config-files/plugins/Bufferline.lua
@@ -1,28 +1,28 @@
 require'bufferline'.setup{
     options = {
-        view = "multiwindow",
+        view = "default",
         numbers = "ordinal",
         number_style = "",
-        mappings = true,
+        mappings = false,
         buffer_close_icon= "",
         modified_icon = "●",
         close_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
-        max_name_length = 30,
+        max_name_length = 40,
         max_prefix_length = 15, -- prefix used when a buffer is deduplicated
-        tab_size = 25,
-        diagnostics = "nvim_lsp",
+        tab_size = 20,
+        diagnostics = false,
         diagnostics_indicator = function(count, level)
             return "("..count..")"
         end,
-        show_buffer_close_icons = true,
+        show_buffer_close_icons = false,
         show_tab_indicators = true,
-        persist_buffer_sort = true,
+        persist_buffer_sort = false,
         -- separator_style = "slant" | "thick" | "thin" | { '▊', '▊' }
         separator_style = "",
         enforce_regular_tabs = false,
         always_show_bufferline = true,
-        sort_by = "directory",
+        sort_by = "",
     }
 }
